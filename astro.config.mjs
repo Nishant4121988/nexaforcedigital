0c8fecece7596597960d6d8a1fc260359b18215c
Nishant4121988 <143177107+Nishant4121988@users.noreply.github.com>
2026-08-01 22:14:30 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://nexaforcedigital.com',
+
   integrations: [
     tailwind(),
-    sitemap({
-      i18n: {
-        defaultLocale: 'en',
-        locales: {
-          en: 'en-GB',
-          de: 'de-DE',
-        },
-      },
-    }),
   ],
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
@@ -23,6 +15,7 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
+
   build: {
     format: 'directory',
   },
